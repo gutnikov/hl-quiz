947f21e1c7e6c77262ea9226898a9f8d104ea8b6
a.gutnikov <a.gutnikov@corp.badoo.com>
2017-09-24 21:19:18 +0300

    quiz question draft (non-working)

---
+++ client/src/QuestionsList.js
@@ -0,0 +1,52 @@
+const questions = [
+	{
+		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		variants: [
+			'adipiscing', // 0
+			'consectetur', // 1
+			'eiusmod', // 2
+			'dolor'	// 3
+		],
+		answer: 1
+	},
+	{
+		text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident',
+		variants: [
+			'veniam',
+			'nostrud',
+			'velit',
+			'laborum'	
+		],
+		answer: 2
+	},
+	{
+		text: 'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+		variants: [
+			'est',
+			'Duis',
+			'Excepteur',
+			'dolore'	
+		],
+		answer: 3
+	}
+];
+
+export default class {
+
+	list;
+	lastQuestionId;
+
+	constructor(props) {
+		super(props);
+		this.list = questions.slice();
+		this.lastQuestionId = -1;
+	}
+
+	getNextQuestion() {
+		// TODO: implement it 
+		if (this.lastQuestionId >= this.list.length) {
+			return null;
+		}
+		return this.list[++this.lastQuestionId];
+	}
+};
--- client/src/Quiz.js
+++ client/src/Quiz.js
@@ -1,17 +1,112 @@
 import React from 'react';
+import QuestionList from './questionList';
+import QuizQuestion from './QuizQuestion';
 
-function Quiz({location}) {
+const QUIZ_TIME_SEC = 20;
+const QUESTION_TIME_SEC = 5;
+const INDICATION_TIME_SEC = 1;
 
-    const match = location.search.match(/p1=(\d+)&p2=(\d+)/);
-    if (match) {
-        const [, p1, p2] = match;
-        return (
-            <div>{`Player ${p1} VS Player ${p2}`}</div>
-        );
-    } else {
-        return <div>Wrong or missing player id</div>;
-    }
+const NO_ANSWER_YET = 0;
+const ANSWER_RIGHT = 1;
+const ANSWER_WRONG = 2;
+const ANSWER_NONE = 3;
 
+class Quiz extends Component {
+
+	questionTimerId = null;
+	indicationTimerId = null;
+
+	rightScoreTo = {
+		1: [1, 0],
+		2: [0, 1]
+	}
+
+	wrongScoreTo = {
+		1: [0, 1],
+		2: [1, 0]
+	}
+
+	constructor(props) {
+		super(props);
+
+	    const match = props.location.search.match(/p1=(\d+)&p2=(\d+)/);
+	    const [, player1Id, player2Id] = match;
+
+		this.state = {
+			question: null,
+			curQuestionTimeLast: null,
+			indicationTimeLast: null
+			// ids
+			player1Id: player1Id,
+			player2Id: player2Id,
+			// scores
+			player1Score: 0,
+			player2Score: 0,
+			// right-wrong indication
+			player1AnswerState: null,
+			player2AnswerState: null,
+
+			error: null,
+		}
+	}
+
+	render() {
+	    if (error) {
+	        return <div>Wrong or missing player id</div>;
+	    }
+
+	    if (!question) {
+	    	return <div>Ready?<div>
+	    }
+
+	    return <QuizQuestion/>
+	}
+
+	run() {
+		this.setNextQuestion();
+	}
+
+	setNextQuestion() {
+		this.questionTimerId = setTimeout(this.onQuestionTimedout.bind(this), QUESTION_TIME_SEC);
+		this.setState({
+			player1AnswerState: NO_ANSWER_YET,
+			player2AnswerState: NO_ANSWER_YET,
+			question: QuestionList.getNextQuestion()
+		});
+	}
+
+	// No one answered?
+	onQuestionTimedout() {
+		this.setState({
+			player1AnswerState: ANSWER_NONE,
+			player2AnswerState: ANSWER_NONE
+		});
+		// Show answer status for a while
+		this.setQuestionPause();
+	}
+
+	// Someone answered?
+	onQuestionAnswer(playerId, isRight) {
+		const [addTo1, addTo2] = (isRight ? this.rightScoreTo : this.wrongScoreTo)[playerId];
+		this.setState({
+			[`player${playerId}AnswerState`] = isRight ? ANSWER_RIGHT : ANSWER_WRONG,
+			player1Score: this.state.player1Score + addTo1,
+			player2Score: this.state.player2Score + addTo2
+		});
+		// Show answer status for a while
+		this.setQuestionPause();
+	}
+
+	// Show answer statuses for a while
+	setQuestionPause() {
+		clearTimeout(this.questionTimerId);
+		this.pauseTimerId = setTimeout(this.onPauseEnded.bind(this));
+	}
+
+	onPauseEnded() {
+		clearTimeout(this.pauseTimerId);
+		this.setNextQuestion();
+	}
 }
 
 export default Quiz;
