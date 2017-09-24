241187df1df7164a2457daf53447baa550d96a34
a.gutnikov <a.gutnikov@corp.badoo.com>
2017-09-24 14:59:36 +0300

    player registration

---
+++ client/src/QuizRegistration.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react';
+import UserLogin from './UserLogin';
+import t from './t';
+import {
+    Link
+} from 'react-router-dom';
+
+function voidUser(id = null) {
+	return  {
+		id: id,
+		name: '',
+		phone: '',
+		error: null,
+		registered: false
+	}
+}
+
+class QuizRegistration extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			user1: voidUser(),
+			user2: voidUser()
+		};
+		this.onSubmit1 = this.onSubmit.bind(this, 1);
+		this.onFieldChange1 = this.onFieldChange.bind(this, 1);
+		this.onSubmit2 = this.onSubmit.bind(this, 2);
+		this.onFieldChange2 = this.onFieldChange.bind(this, 2);
+	}
+
+	render() {
+		const bothRegistered = this.state.user1.registered && this.state.user2.registered;
+		if (bothRegistered) {
+			return <Link to={`/quiz?p1=${this.state.user1.id}&p2=${this.state.user2.id}`}>{t('Fight')}</Link>;
+		} else {
+	        return <div>
+	        	<UserLogin user={this.state.user1} onSubmit={this.onSubmit1} onFieldChange={this.onFieldChange1}/>
+	        	<UserLogin user={this.state.user2} onSubmit={this.onSubmit2} onFieldChange={this.onFieldChange2}/>
+	        </div>;
+		}
+	}	
+
+	onFieldChange(formId, event) {
+		const {
+			name,
+			value
+		} = event.target;
+		this.setState({
+			[`user${formId}`]: Object.assign({}, this.state[`user${formId}`], {
+				[name] : value
+			})
+		});
+	}
+
+	onSubmit(formId) {
+		// Try to register user...
+		fetch('http://localhost:8080/player', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: this.state[`user${formId}`].name,
+				phone: this.state[`user${formId}`].phone
+			})
+		}).then(function(res) {
+			switch (res.status) {
+				case 400:
+					this.setError(formId, t('Form is invalid'));
+					break;
+				case 409:
+					this.setError(formId, t('User already registered'));
+					break;
+				case 201:
+					res.json().then(function(json) {
+						this.setState({
+							[`user${formId}`]: Object.assign({}, this.state[`user${formId}`], {
+								id: json.id,
+								error: null,
+								registered: true
+							})
+						});
+					}.bind(this));
+					break;
+				default:
+					this.setError(formId, t('Response code is not supported') + `:${res.status}`);
+					break;
+			}
+		}.bind(this));
+	}
+
+	setError(formId, error) {
+		this.setState({
+			[`user${formId}`]: Object.assign({}, this.state[`user${formId}`], {
+				error: error
+			})
+		});
+	}
+
+}
+
+export default QuizRegistration;
---
+++ client/src/UserLogin.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import t from './t';
+
+function UserLogin(props) {
+	const {
+		name,
+		phone,
+		error,
+		registered
+	} = props.user;
+	const canSubmit = name && name.trim().length && phone && phone.trim().length;
+
+	if (registered) {
+		return <div>
+			<span>{t('Name')}: {name}</span>
+			<span>{t('Phone')}: {phone}</span>
+			<span>{t('Registered')}!</span>;
+		</div>;
+	} else {
+        return <div>
+        	<div>{error || ''}</div>
+        	<label>{t('Name')}: <input type="text" value={name} name="name" onChange={props.onFieldChange}/></label>
+        	<label>{t('Phone')}: <input type="text" value={phone} name="phone" onChange={props.onFieldChange}/></label>
+        	<button type="button" onClick={props.onSubmit} disabled={canSubmit ? null : 'disabled'}>{t('Done')}</button>
+        </div>;
+	}
+}
+
+export default UserLogin;
