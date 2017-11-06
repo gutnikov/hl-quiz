5a47404f52b5136e506604191d28ea9520e72114
GitKeeper <info@sfera-pr.ru>
2017-11-06 01:25:28 +0000

    fix score-results render

--- client/src/QuizScore.js
+++ client/src/QuizScore.js
@@ -2,7 +2,7 @@ import React from 'react';
 import t from './t';
 
 function QuizScore({player1, player2, player1Score, player2Score}) {
-	return (<div className="quiz-score d-flex align-items-center">
+	return (<div className="quiz-score d-flex align-items-center justify-content-center">
 		<div className="player-score">
 			<div className="card animated flipInX">
 				<h2 className="display-2 p-3">{player1Score}</h2>
