76b513a910c0aab5943a3dabedf7c83667316858
GitKeeper <info@sfera-pr.ru>
2017-11-07 04:59:27 +0000

    carousel - commented - not ready yet

--- client/src/PlayerList.js
+++ client/src/PlayerList.js
@@ -18,6 +18,38 @@ class PlayerList extends Component {
                 <div className="col-12 animated fadeIn">
                     <div className="card">
                         <div className="card-body">
+                            {/* <div id="myCarousel" className="carousel slide" data-ride="carousel">
+            
+                                <ol className="carousel-indicators">
+                                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                                </ol>
+
+                                <div className="carousel-inner">
+                                <div className="item active">
+                                    <img src="/img/slider/1-1/png" alt="" />
+                                </div>
+                            
+                                <div className="item">
+                                    <img src="/img/slider/1-2/png" alt="" />
+                                </div>
+                            
+                                <div className="item">
+                                <img src="/img/slider/1/png" alt="" />
+                                </div>
+                            </div>
+          
+                            <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                                <span className="glyphicon glyphicon-chevron-left"></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                                <span className="glyphicon glyphicon-chevron-right"></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                        </div> */}
+                            
                             <h1 className="display-4 font-bold mb-5 Up">Рейтинг игроков</h1>
                             <table className="table secondary-color-dark text-white text-center animated zoomIn">
                                 <thead>
